25b4c686ec49a66b52f70011f082200a26a391ef
Gideone Clementino <gideonesilva66@gmail.com>
2020-11-28 15:57:41 -0300

    Adicionando interações e buscando informações

--- script.js
+++ script.js
@@ -6,10 +6,74 @@ let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
+//Variaveis de ambiente
+let etapaAtual = 0;
+let numero = '';
 
 //Funções
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = '';
+    numero = '';
+
+    for(let i = 0; i < etapa.numeros; i++) {
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';    
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }    
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface(){
+    console.log("Atualizando interface");
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        }else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for(let i in candidato.fotos){
+            fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
+
+    console.log("Candidato", candidato);
+}
+
 function clicou(n){
-    alert("Clicou em " + n);
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null){
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }    
+    }
 }
 
 function branco() {
@@ -17,9 +81,11 @@ function branco() {
 }
 
 function corrige() {
-    alert("Clicou em CORRIGE");
+    
 }
 
 function confirma() {
     alert("Clicou em CONFIRMA");
 }
+
+comecarEtapa();
